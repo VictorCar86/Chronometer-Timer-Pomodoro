3b977974edbd80c28f0fd9362e7e670be0ba7ed8
Victor Carreno <vmanuelcar@hotmail.com>
2022-06-17 12:38:44 -0500

    Timer finished

--- src/index.js
+++ src/index.js
@@ -8,13 +8,44 @@ const timerHoursDigits = document.getElementById("hours_id");
 const timerMinutesDigits = document.getElementById("minutes_id");
 const timerSecondsDigits = document.getElementById("seconds_id");
 const timerMilisecondsDigits = document.getElementById("miliseconds_id");
+const timerInputHours = document.getElementById("inputHours");
+const timerInputMinutes = document.getElementById("inputMinutes");
+const timerInputSeconds = document.getElementById("inputSeconds");
 
+// Main variables
 let intervalFunction;
 let currentButton = false;
-let hoursCount = 3;
+let timerNumbersStarted = false;
+let hoursCount = 0;
 let minutesCount = 0;
-let secondsCount = 3;
-let milisecondsCount = 60;
+let secondsCount = 0;
+let milisecondsCount = 0;
+
+// Utils
+function addZeros(textToChange, counter){
+    if (counter < 10){
+        if (counter == -1){
+            textToChange.innerText = "00"
+        } else {
+            textToChange.innerText = "0" + counter
+        }
+    } else {
+        textToChange.innerText = counter
+    }
+}
+
+function checkCorrectNumbers(input){
+    input.onkeydown = (num) => {
+        if(!((num.keyCode > 95 && num.keyCode < 106)
+          || (num.keyCode > 47 && num.keyCode < 58)
+          || num.keyCode == 8)) {
+            return false;
+        }
+    }
+}
+checkCorrectNumbers(timerInputHours)
+checkCorrectNumbers(timerInputMinutes)
+checkCorrectNumbers(timerInputSeconds)
 
 // Chronometer
 function startChronometer(){
@@ -65,53 +96,70 @@ function resetChronometer(){
     chronoMilisecondsDigits.innerText = "00"
 }
 
-function addZeros(textToChange, counter){
-    if (counter < 10){
-        textToChange.innerText = "0" + counter
-    } else {
-        textToChange.innerText = counter
-    }
-}
-
 // Timer
 function startTimer(){
-    if (!currentButton){
-        currentButton = true
-        intervalFunction = setInterval(()=>{
-            milisecondsCount--
-            console.log(milisecondsCount)
-            addZeros(timerMilisecondsDigits, milisecondsCount)
-            if (milisecondsCount == 0){
-                secondsCount--
+    if (!timerNumbersStarted){
+        hoursCount = Number(timerInputHours.value)
+        timerHoursDigits.innerHTML = "0" + hoursCount
+        minutesCount = Number(timerInputMinutes.value)
+        timerMinutesDigits.innerHTML = "0" + minutesCount
+        secondsCount = Number(timerInputSeconds.value)
+        timerSecondsDigits.innerHTML = "0" + secondsCount
+    }
+    if (!hoursCount <= 0 || !minutesCount <= 0 || !secondsCount <= 0){
+        if (!currentButton){
+            currentButton = true
+            timerNumbersStarted = true
+
+            intervalFunction = setInterval(()=>{
+                milisecondsCount--
+                addZeros(timerMilisecondsDigits, milisecondsCount)
                 addZeros(timerSecondsDigits, secondsCount)
-                milisecondsCount = 99
-            }
-            if (secondsCount == 0 && milisecondsCount == 1){
-                minutesCount--
                 addZeros(timerMinutesDigits, minutesCount)
-                milisecondsCount = 99
-                timerSecondsDigits.innerText = 59
-                secondsCount = 59
-            }
-            if (minutesCount == 0 && secondsCount == 0 && milisecondsCount == 2){
-                hoursCount--
                 addZeros(timerHoursDigits, hoursCount)
-                milisecondsCount = 99
-                timerMinutesDigits.innerText = 59
-                minutesCount = 59
-                timerSecondsDigits.innerText = 59
-                secondsCount = 59
-            }
-        }, 10)
+                if (milisecondsCount < 0){
+                    secondsCount--
+                    addZeros(timerSecondsDigits, secondsCount)
+                    milisecondsCount = 99
+                }
+                if (secondsCount == -1){
+                    minutesCount--
+                    addZeros(timerMinutesDigits, minutesCount)
+                    milisecondsCount = 99
+                    timerSecondsDigits.innerText = 59
+                    secondsCount = 59
+                }
+                if (minutesCount == -1){
+                    hoursCount--
+                    addZeros(timerHoursDigits, hoursCount)
+                    milisecondsCount = 99
+                    timerMinutesDigits.innerText = 59
+                    minutesCount = 59
+                    timerSecondsDigits.innerText = 59
+                    secondsCount = 59
+                }
+                if (hoursCount == 0 && minutesCount == 0 && secondsCount == 0 && milisecondsCount == 0){
+                    resetTimer()
+                    alert("Timer finished")
+                }
+            }, 10)
+        }
+    } else {
+        alert("Insert a number to continue")
+        timerHoursDigits.innerText = "00"
+        timerMinutesDigits.innerText = "00"
+        timerSecondsDigits.innerText = "00"
+        timerMilisecondsDigits.innerText = "00"
     }
 }
 
 function resetTimer(){
     stopCounting()
-    hoursCount = 0;
-    minutesCount = 0;
-    secondsCount = 0;
-    milisecondsCount = 0;
+    timerNumbersStarted = false
+    hoursCount = 0
+    minutesCount = 0
+    secondsCount = 0
+    milisecondsCount = 0
     timerHoursDigits.innerText = "00"
     timerMinutesDigits.innerText = "00"
     timerSecondsDigits.innerText = "00"
